e93b4e06ed4958f4ff74204fbdfca5d98bf41d5b
GoldenIShark <ahmadytta02@gmail.com>
2025-12-29 16:11:31 +0700

    Update firebase-messaging-sw.js

--- HOMEPAGE/firebase-messaging-sw.js
+++ HOMEPAGE/firebase-messaging-sw.js
@@ -2,11 +2,11 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "API_KEY_KAMU",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyD1ov5494poj9d1homIMhUajAvTvuf2W9M",
+  authDomain: "notifikasi-website-goldenshark.firebaseapp.com",
+  projectId: "notifikasi-website-goldenshark",
+  messagingSenderId: "20732522648",
+  appId: "1:20732522648:web:cd18913a4f3636cdd6a0ed"
 });
 
 firebase.messaging();
